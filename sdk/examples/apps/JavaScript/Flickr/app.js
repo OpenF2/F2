d7e5fa0d1392e822e9660aff1b31649919cd81cb
markhealey <mark.healey@markit.com>
2012-08-27 23:35:33 -0600

    Updates to Flickr example app to support About view.

--- sdk/examples/apps/JavaScript/Flickr/app.js
+++ sdk/examples/apps/JavaScript/Flickr/app.js
@@ -4,16 +4,35 @@ App_Class = function (app, appAssets) {
 };
 
 App_Class.prototype.init = function () {
-
+	F2.log(this)
 	this._container = $("#" + this._app.instanceId);
 	this._app.updateHeight();
 	
-	console.log("¡Vamanos! Flickr...");
+	this.bindEvents();
+
+	console.log("¡Vámonos! Flickr interestingness coming up...");
+
 	this.getPhotos();
 };
 
-App_Class.prototype._handleViewChange = function() {
+App_Class.prototype.bindEvents = function(){
+	var self = this;
 
+	// bind 'about this app' change event
+	F2.Events.on(F2.Constants.Events.APP_VIEW_CHANGE + this._app.instanceId, $.proxy(this._handleViewChange, this));
+
+	//bind 'back to home' event 
+	$("div[data-f2-view='about'] a", this._container).bind("click",function(e){
+		self._handleViewChange("home");
+	});
+}
+
+App_Class.prototype._handleViewChange = function(view) {
+
+	$("div." + F2.Constants.Css.APP_VIEW, this._container).addClass("hide");
+	$("div." + F2.Constants.Css.APP_VIEW + "[data-f2-view='"+view+"']", this._container).removeClass("hide");
+	
+	this._app.updateHeight();
 };
 
 App_Class.prototype.getPhotos = function(){
@@ -25,19 +44,19 @@ App_Class.prototype.getPhotos = function(){
 		context:this
 	}).done(function(jqxhr,txtStatus){
 		//F2.log(jqxhr)
-		this.loadPhoto(jqxhr.photos.photo[0])
+		this.loadPhoto(jqxhr.photos.photo[0]);
 	}).fail(function(jqxhr,txtStatus){
-		
+		$("div.imgPlaceholder", this._container.selector).html("Booof! Flickr or something failed.");
 	});
 }
 
 App_Class.prototype.loadPhoto = function(photo){
 	var url = this.makePhotoURL(photo);
 	//F2.log(url);
-	$("#imgPlaceholder").html("<img src='"+url+"' class='img-polaroid' >");
+	$("div.imgPlaceholder", this._container).html("<img src='"+url+"' class='img-polaroid' >");
 }
 
-//http://www.flickr.com/services/api/misc.urls.html
 App_Class.prototype.makePhotoURL = function(photo){
+	//see: http://www.flickr.com/services/api/misc.urls.html
 	return ["http://farm", photo.farm, ".staticflickr.com/", photo.server, "/",photo.id, "_" ,photo.secret, ".jpg"].join("");
 }
--- sdk/examples/apps/JavaScript/Flickr/resources.js
+++ sdk/examples/apps/JavaScript/Flickr/resources.js
@@ -20,7 +20,7 @@ containerToApp_3493749374339473947394397439473972018({
    "Apps":[
       {
          "InstanceId":"f3703684-a71a-42c2-acde-cfba8fc484c1",
-         "Html":"<div class=well><p>Hello Container. I'm 100% JavaScript talking to Flickr's <a href='http://www.flickr.com/services/api/flickr.interestingness.getList.html'>Interestingness API</a>.</p><div id='imgPlaceholder' class='thumbnail'></div></div>"
+         "Html":"<div class=well><div class='f2-app-view' data-f2-view='home'><p>Hello Container. I'm 100% JavaScript talking to Flickr's <a href='http://www.flickr.com/services/api/flickr.interestingness.getList.html'>Interestingness API</a>.</p><div class='imgPlaceholder thumbnail'></div></div><div class='f2-app-view hide' data-f2-view='about'><h3>About</h3><p>This is a pretty basic app that talks to Flickr's interestingness API and shows the most recent photograph.</p><p><a href='#' class=back>&laquo; Back</a></div></div>"
       }
    ]
 })
