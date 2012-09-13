806c0dc5464b7ac2ab2d2ba8f312cee184958c44
brianbaker <brian.baker@markit.com>
2012-09-13 13:19:57 -0600

    fixing hash tag references

--- sdk/docs/assets/js/main.js
+++ sdk/docs/assets/js/main.js
@@ -1,22 +1,24 @@
-$(function() {
+var SDK_Manager = function() {
+
+};
+
+SDK_Manager.prototype.init = function() {
+
+	// optionally apply .affix() if the window height is big enough
 	var winHeight = $(window).height();
 	var tocHeight = $('#toc').outerHeight() + $('#toc').position().top;
 	if (winHeight > tocHeight + 200) {
 		$('#toc ul.nav').affix();
 	}
 
-	// go to correct class members tab
-	var activeTab = $('a[href="' + location.hash + '"]');
-	activeTab && activeTab.tab('show');
+	// bind hashchange and go to tab
+	$(window).on('hashchange', $.proxy(this._handleHashChange, this));
+	this._handleHashChange();
 
 	// class members
-	$('table.table-members').on('click', 'a[data-tab]', function() {
-		var tab = $(this).data('tab');
-		var tabMember = $(this).data('tab-member');
-		$('#classTabs [href="' + tab + '"]').tab('show');
-		var offset = $(tab + '-' + tabMember).offset();
-		$('html, body').animate({scrollTop:offset.top - 75});
-	});
+	//$('table.table-members').on('click', 'a[data-tab]', $.proxy(function(event) {
+	//	this._scrollToClassMember($(event.currentTarget).data('tab'), $(event.currentTarget).data('tab-member'));
+	//}, this));
 
 	// view options
 	$('#viewOptions input')
@@ -26,9 +28,47 @@ $(function() {
 		.on('click', function() {
 			$('#main').toggleClass('hide-' + $(this).val(), !$(this).is(':checked'));
 		});
-	
+
 	// back to top
 	$('#doc').on('click', '.backToTop a', function() {
 		$('html, body').animate({scrollTop:0});
 	});
+};
+
+SDK_Manager.prototype._handleHashChange = function() {
+
+	if (!!location.hash) {
+		var hashParts = String(location.hash).split('-');
+
+		// handle class member
+		if ($('#classTabs a[href="' + hashParts[0] + '"]').length) {
+			this._scrollToClassMember(hashParts[0], hashParts[1]);
+
+		// handle line number
+		} else if (/^#l\d+$/.test(hashParts[0])) {
+			var line = parseInt(hashParts[0].substring(2));	
+			var offset = $('ol.linenums li').eq(line).offset();
+			$('html, body').animate({scrollTop:offset.top - 75});
+
+		// everything else
+		} else {
+			var offset = $(hashParts[0]).offset();
+			$('html, body').animate({scrollTop:offset.top - 75});
+		}
+	}
+};
+
+SDK_Manager.prototype._scrollToClassMember = function(tab, member) {
+
+	$('#classTabs [href="' + tab + '"]').tab('show');
+
+	if (member) {
+		var offset = $(tab + '-' + member).offset();
+		$('html, body').animate({scrollTop:offset.top - 75});
+	}
+};
+
+$(function() {
+	var sdk = new SDK_Manager();
+	sdk.init();
 });
