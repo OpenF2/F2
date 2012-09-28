9c8fecda47350204ae97803c8d572094d5ad5d9d
brianbaker <brian.baker@markit.com>
2012-09-28 12:10:44 -0600

    updated styles

--- examples/apps/JavaScript/Quote/appclass.js
+++ examples/apps/JavaScript/Quote/appclass.js
@@ -1,40 +1,14 @@
 F2.Apps['com_openf2_examples_javascript_quote'] = function (appConfig, appContent, root) {
 
 	var $root = $(root);
-	var $quote = $('div.quote', $root);
+	var $caption = $('caption', $root);
+	var $tbody = $('tbody', $root);
 	var $settings = $('form[data-f2-view="settings"]', $root);
 	var _config = {
 		refreshMode: 'page',
 		autoRefresh: false
 	};
-	var _defaultFormatOptions = {
-		precision: 2,
-		withColors: false,
-		prefix: '',
-		suffix: ''
-	};
 	var _autoRefreshInterval = false;
-
-	/**
-	 * Formats a number
-	 * @method _formatNumber
-	 * @param {number} raw The number to format
-	 * @param {object|int} [options] If int, formats to X precision. If object, 
-	 * formats according to options passed
-	 */
-	var _formatNumber = function(raw, options) {
-		if (!raw) { return '--'; }
-
-		options = typeof options === 'number' ? { precision: options } : options;
-		options = $.extend({}, _defaultFormatOptions, options);
-
-		var val = raw.toFixed(options.precision);
-		val = options.prefix + val + options.suffix;
-
-		return !!options.withColors
-			? ('<span class="' + (raw > 0 ? 'positive' : raw < 0 ? 'negative' : 'unchanged') + '">' +  val + '</span>')
-			: val;
-	};
 
 	var _getQuote = function(symbolData) {
 
@@ -96,33 +70,45 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function (appConfig, appConten
 	var _renderQuote = function(quoteData) {
 
 		if (quoteData && quoteData.Data && quoteData.Data.Status == 'SUCCESS') {
-			appConfig.ui.setTitle('Quote for ' + quoteData.Data.Name);
+			appConfig.ui.setTitle(quoteData.Data.Name);
 
-			$quote.fadeOut().promise().done(function() {
+			$caption.fadeOut().promise().done(function() {
 				$(this)
 					.empty()
 					.append([
-						'<h3 class="last">', _formatNumber(quoteData.Data.LastPrice, 2), '</h3>',
-						'<h4 class="lastChange">', _formatNumber(quoteData.Data.Change, {precision:2, withColors:true}), ' ', _formatNumber(quoteData.Data.ChangePercent, {precision:2, withColors:true, prefix:'(', suffix:'%)'}), '</h4>',
-						'<table class="table table-condensed">',
-							'<tr>',
-								'<th>Range</th>',
-								'<td>', _formatNumber(quoteData.Data.Low), ' - ', _formatNumber(quoteData.Data.High), '</td>',
-							'</tr>',
-							'<tr>',
-								'<th>Open</th>',
-								'<td>', _formatNumber(quoteData.Data.Open), '</td>',
-							'</tr>',
-							'<tr>',
-								'<th>Volume</th>',
-								'<td>', quoteData.Data.Volume, '</td>',
-							'</tr>',
-							'<tr>',
-								'<th>Market Cap</th>',
-								'<td>', quoteData.Data.MarketCap, '</td>',
-							'</tr>',
-						'</table>',
-						'<button type="button" class="btn btn-mini" data-watchlist-add="',quoteData.Data.Symbol,'">Add ',quoteData.Data.Symbol,' to Watchlist App</button>'
+						'<h3>',
+							'<span class="last pull-left">', Format.number(quoteData.Data.LastPrice, 2), '</span>',
+							'<span class="last-change pull-right">', Format.number(quoteData.Data.Change, {precision:2, withColors:true}), ' ', Format.number(quoteData.Data.ChangePercent, {precision:2, withColors:true, prefix:'(', suffix:'%)'}), '</span>',
+						'</h3>'
+					].join(''))
+					.fadeIn();
+			});
+
+			$tbody.fadeOut().promise().done(function() {
+				$(this)
+					.empty()
+					.append([
+						'<tr>',
+							'<th>Range</th>',
+							'<td><strong>', Format.number(quoteData.Data.Low), ' - ', Format.number(quoteData.Data.High), '</strong></td>',
+						'</tr>',
+						'<tr>',
+							'<th>Open</th>',
+							'<td><strong>', Format.number(quoteData.Data.Open), '</strong></td>',
+						'</tr>',
+						'<tr>',
+							'<th>Volume</th>',
+							'<td><strong>', Format.number(quoteData.Data.Volume, {withMagnitude:true,precision:1}), '</strong></td>',
+						'</tr>',
+						'<tr>',
+							'<th>Market Cap</th>',
+							'<td><strong>', Format.number(quoteData.Data.MarketCap, {withMagnitude:true,precision:1}), '</strong></td>',
+						'</tr>',
+						'<tr>',
+							'<td colspan="2">',
+								'<a href="javascript:;" class="pull-right" data-watchlist-add="', quoteData.Data.Symbol, '">+Add ', quoteData.Data.Symbol, ' to Watchlist</a>',
+							'</td>',
+						'</tr>',
 					].join(''))
 					.fadeIn();
 			});
@@ -149,6 +135,59 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function (appConfig, appConten
 
 		appConfig.ui.Views.change(F2.Constants.Views.HOME);
 	};
+
+	/**
+	 * @class Format
+	 * @static
+	 */
+	var Format = (function() {
+		var _defaultOptions = {
+			precision: 2,
+			withColors: false,
+			withMagnitude: false,
+			prefix: '',
+			suffix: ''
+		};
+		var _magnitudes = {
+			'shortcap': ['', 'K', 'M', 'B', 'T']
+		};
+
+		return {
+			/**
+			 * Formats a number
+			 * @method number
+			 * @param {number} raw The number to format
+			 * @param {object|int} [options] If int, formats to X precision. If
+			 * object, formats according to options passed
+			 */
+			number:function(raw, options) {
+				if (!raw) { return '--'; }
+
+				options = typeof options === 'number' ? { precision: options } : options;
+				options = $.extend({}, _defaultOptions, options);
+
+				var val;
+
+				if (options.withMagnitude) {
+					var c = 0;
+					raw = Math.abs(raw);
+					while (raw >= 1000 && c < 4) {
+						raw /= 1000;
+						c++;
+					}
+					options.magnitudeType = options.magnitudeType || 'shortcap';
+					options.suffix = _magnitudes[options.magnitudeType][c];
+				}
+					
+				val = raw.toFixed(options.precision);
+				val = options.prefix + val + options.suffix;
+
+				return !!options.withColors
+					? ('<span class="' + (raw > 0 ? 'positive' : raw < 0 ? 'negative' : 'unchanged') + '">' +  val + '</span>')
+					: val;
+			}
+		};
+	})();
 
 	return {
 		init: function() {
@@ -180,7 +219,7 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function (appConfig, appConten
 			});
 
 			//Talk to External Watchlist App
-			$root.on("click", "button[data-watchlist-add]", function(e){
+			$root.on("click", "a[data-watchlist-add]", function(e){
 				var $this = $(e.currentTarget),
 					sym = $this.attr("data-watchlist-add");
 
