12c1fbce8f8a33a8b08edcb714a0a0d5adf09e22
markhealey <mark.healey@markit.com>
2012-10-15 11:42:59 -0600

    Fixes chart in IE #1

--- examples/apps/JavaScript/Chart/appclass.js
+++ examples/apps/JavaScript/Chart/appclass.js
@@ -154,12 +154,14 @@ F2.Apps["com_openf2_examples_javascript_chart"] = (function(){
 		else downSeriesData.push( [ prevJSUTCDate, minVal ], [ currentJSUTCDate, minVal ] );
 
 		// Add the up month/down month data to the chart's series
-
-		//GH issue #1
-		//hcChartObj.series[1].setData(upSeriesData, false);
-		//hcChartObj.series[2].setData(downSeriesData, false);
-
-		hcChartObj.yAxis[0].setExtremes(dataRanges.dataMin, dataRanges.dataMax, true, false);
+        if ($.browser.msie && parseInt($.browser.version) < 9) {
+            //commenting out for issue #1. 
+            //o-do to come back and get grid lines working.
+        } else {
+            hcChartObj.series[1].setData(upSeriesData, false);
+            hcChartObj.series[2].setData(downSeriesData, false);
+        }
+        hcChartObj.yAxis[0].setExtremes(dataRanges.dataMin, dataRanges.dataMax, true, false);
 
 		this.ui.updateHeight();
 	};
