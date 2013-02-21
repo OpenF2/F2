3e2a0aac661341b82174604d393a4dbf39fadb6d
Brian Baker <brian.baker@markit.com>
2013-02-21 16:48:20 -0700

    added amd-specific jasmine test page

---
+++ tests/spec/amd-spec.js
@@ -0,0 +1,22 @@
+describe('AMD', function() {
+
+	it('should define F2', function() {
+
+		var isLoaded = false;
+
+		require(["../sdk/f2.debug.js"], function (nonGlobalF2) {
+			isLoaded = typeof nonGlobalF2 !== "undefined";
+		});
+
+		waitsFor(function() {
+			//console.log('ping');
+			return isLoaded;
+		}, 'F2-AMD never loaded', 10000);
+
+		runs(function() { });
+	});
+
+	it('should still globally define F2', function() {
+		expect(F2).toBeDefined();
+	});
+});
