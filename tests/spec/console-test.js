022b104e678f4aa4479c15bb0ddeaba31485d9ec
Ali Khatami <Ali.Khatami@markit.com>
2013-03-21 16:23:26 -0600

    Adding some test files/changes to test integration.

---
+++ tests/spec/console-test.js
@@ -0,0 +1,9 @@
+describe("Console", function() {  
+  it ("should fail", function() {
+    expect(false).toBeTruthy();
+  });
+  
+  it ("should succeed", function() {
+    expect(true).toBeTruthy();
+  });
+});
