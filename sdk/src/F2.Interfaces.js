6408cab46d545262c2d2fe7cd26921c9d2480b3c
Chris Montrois <chris.montrois@markit.com>
2013-10-08 13:32:23 -0600

    Removing 'create' function.

--- sdk/src/F2.Interfaces.js
+++ sdk/src/F2.Interfaces.js
@@ -74,73 +74,6 @@
 		}
 	};
 
-	function _getValueFromType(type) {
-		switch (type) {
-			case 'array':
-				return [];
-			case 'boolean':
-				return false;
-			case 'integer':
-			case 'number':
-				return 0;
-			case 'object':
-				return {};
-			case 'string':
-				return '';
-		}
-
-		return null;
-	}
-
-	function _createSchemaInstance(schema) {
-		var obj;
-
-		if (schema.properties) {
-			obj = {};
-
-			for (var prop in schema.properties) {
-				var def = schema.properties[prop];
-				var value;
-
-				// Use the default if it's available
-				if (typeof def['default'] !== 'undefined' && def['default'] !== 'any') {
-					value = def['default'];
-				}
-				else {
-					if (def.type === 'object' && def.properties) {
-						// Recurse down into oblivion
-						value = _createSchemaInstance(def);
-					}
-					else if (def.type === 'array' && def.minItems > 0) {
-						value = _getValueFromType(def.type);
-
-						// Make a new instance of each required item
-						if (def.items) {
-							for (var i = 0; i < def.minItems; i++) {
-								value.push(_createSchemaInstance(def.items));
-							}
-						}
-						else {
-							// Populate the array with undefined
-							// TODO: figure out if we want to add undefineds
-							value.length = def.minItems;
-						}
-					}
-					else {
-						value = _getValueFromType(def.type);
-					}
-				}
-
-				obj[prop] = value;
-			}
-		}
-		else {
-			obj = _getValueFromType(schema.type);
-		}
-
-		return obj;
-	}
-
 	return {
 		validate: function(json, nameOrSchema) {
 			if (!nameOrSchema) {
@@ -168,18 +101,6 @@
 			}
 
 			schemas[name] = schema;
-		},
-		create: function(schema) {
-			if (!schema) {
-				throw 'F2.Interfaces: you must provide a schema or schema name.';
-			}
-
-			// Grab the actual schema if they passed in a string
-			if (schemas[schema]) {
-				schema = schemas[schema];
-			}
-
-			return _createSchemaInstance(schema);
 		},
 		schemas: schemas
 	};
