7b4adc2d08861095097f05b4761d82eb2a1fbba9
Mark Healey <mark.healey@markit.com>
2013-10-17 09:14:34 -0600

    Updating /tests dependencies

--- tests/spec/misc-spec.js
+++ tests/spec/misc-spec.js
@@ -4,7 +4,7 @@ describe('misc', function() {
 		// the AMD test page does not include jquery
 		if (typeof $ !== 'undefined') {
 			// the jasmine test page loads 1.8.2 while 1.8.3 is bundled with F2
-			expect($.fn.jquery).toEqual('1.8.2');
+			expect($.fn.jquery).toEqual('1.10.2');
 		}
 	});
 
