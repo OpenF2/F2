7be56a3b91b43fbe5e6784aa4063cbc496c5f65d
Mark Healey <mark.healey@markit.com>
2013-10-17 11:57:36 -0600

    Adding version detail to EventEmitter2 #106

--- sdk/src/third-party/eventemitter2.js
+++ sdk/src/third-party/eventemitter2.js
@@ -5,6 +5,11 @@
  * 
  * http://www.twitter.com/hij1nx
  * 
+ * Version: 2013-09-17
+ * GitHub SHA: 3caacce662998d7903d368b0c0f847f259cae0f7
+ * https://github.com/hij1nx/EventEmitter2
+ * Diff this version to master: https://github.com/hij1nx/EventEmitter2/compare/3caacce662998d7903d368b0c0f847f259cae0f7...master
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
  * documentation files (the 'Software'), to deal in the Software without restriction, including without limitation 
  * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
