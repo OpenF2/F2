f3f5284b46dd132bcba02c05c1445a405982041a
Mark Healey <mark.healey@markit.com>
2013-10-17 09:19:23 -0600

    Downgrading /tests jQuery version for "misc" specs

--- tests/spec/misc-spec.js
+++ tests/spec/misc-spec.js
@@ -3,8 +3,8 @@ describe('misc', function() {
 	it('should not overwrite existing jQuery versions', function() {
 		// the AMD test page does not include jquery
 		if (typeof $ !== 'undefined') {
-			// the jasmine test page loads 1.8.2 while 1.8.3 is bundled with F2
-			expect($.fn.jquery).toEqual('1.10.2');
+			// the jasmine test page loads 1.10.1 while 1.10.2 is bundled with F2
+			expect($.fn.jquery).toEqual('1.10.1');
 		}
 	});
 
