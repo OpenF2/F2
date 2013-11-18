c230b965533d4955b7683283d8ead8379f560122
Mark Healey <mark.healey@markit.com>
2013-11-18 10:43:34 -0700

    1.3.2 rc1

--- sdk/packages/f2.no-bootstrap.js
+++ sdk/packages/f2.no-bootstrap.js
@@ -13062,7 +13062,7 @@ global.easyXDM = easyXDM;
 })(window, document, location, window.setTimeout, decodeURIComponent, encodeURIComponent);
 
 /*!
- * F2 v1.3.2 11-12-2013
+ * F2 v1.3.2 11-18-2013
  * Copyright (c) 2013 Markit On Demand, Inc. http://www.openf2.org
  *
  * "F2" is licensed under the Apache License, Version 2.0 (the "License"); 
