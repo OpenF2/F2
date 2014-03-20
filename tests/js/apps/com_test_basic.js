1e92ee4676202577b31f29fef40d2665559447c1
Chris Montrois <chris@chrismontrois.net>
2014-03-20 12:44:58 -0600

    Removing logs.

--- tests/js/apps/com_test_basic.js
+++ tests/js/apps/com_test_basic.js
@@ -1,9 +1,6 @@
 ﻿define('com_test_basic', ['F2'], function(F2) {
 
-	console.log("----- inside define");
-
 	function AppClass(instanceId, appConfig, data, root) {
-		console.log("----- inside app class");
 		this.instanceId = instanceId;
 		this.appConfig = appConfig;
 		this.data = data;
