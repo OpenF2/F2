dc7d854f0aef27f394d5e3ae4ab49e0b7622bed7
brianbaker <brian.baker@markit.com>
2014-04-30 12:22:09 -0600

    Recompiled JS after merge

--- sdk/f2.debug.js
+++ sdk/f2.debug.js
@@ -13638,7 +13638,7 @@ global.easyXDM = easyXDM;
 })(window, document, location, window.setTimeout, decodeURIComponent, encodeURIComponent);
 
 /*!
- * F2 v1.4.0 01-23-2014
+ * F2 v1.4.0 04-30-2014
  * Copyright (c) 2014 Markit On Demand, Inc. http://www.openf2.org
  *
  * "F2" is licensed under the Apache License, Version 2.0 (the "License"); 
@@ -14002,7 +14002,7 @@ F2 = (function() {
 		 * @method version
 		 * @return {string} F2 version number
 		 */
-		version: function() { return '1.3.3'; }
+		version: function() { return '1.4.0'; }
 	};
 })();
 
