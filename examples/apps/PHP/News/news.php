4e90dcf64a309f6ff224a366031fe45cd3b319de
Mark Healey <mark.healey@markit.com>
2014-11-18 17:49:05 -0700

    container-amd/ cleanup and bs3

--- examples/apps/PHP/News/news.php
+++ examples/apps/PHP/News/news.php
@@ -115,23 +115,23 @@ HTML;
 
 		foreach ($PROVIDERS as $key => $value) {
 			$providerHtml[] = join('', array(
-				'<label class="radio">',
+				'<div class="radio"><label>',
 					'<input type="radio" name="provider" value="', $key, '" ', ($key == $provider ? 'checked' : '') ,'> ',
 					$value['display'],
-				'</label>'
+				'</label></div>'
 			));
 		}
 
 		$html = array(
 			'<form class="f2-app-view hide" data-f2-view="settings">',
-				'<label class="checkbox" name="autoRefresh">',
+				'<div class="checkbox" name="autoRefresh"><label>',
 					'<input type="checkbox" name="autoRefresh"> 30-Second Auto-Refresh',
-				'</label>',
+				'</label></div>',
 				'<span class="help-block">News Provider:</span>',
 				join('', $providerHtml),
 				'<div class="form-actions">',
 					'<button type="button" class="btn btn-primary save">Save</button> ',
-					'<button type="button" class="btn cancel">Cancel</button>',
+					'<button type="button" class="btn btn-default cancel">Cancel</button>',
 				'</div>',
 			'</form>'
 		);
