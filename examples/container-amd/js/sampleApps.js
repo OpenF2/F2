900d317ac4dd501dbbb6b897b1c380a193b600aa
Mark Healey <mark.healey@markit.com>
2014-11-24 13:05:12 -0700

    bug squishing, fixes #189

--- examples/container-amd/js/sampleApps.js
+++ examples/container-amd/js/sampleApps.js
@@ -9,11 +9,6 @@
 
 	"js": [
 		{
-			"appId": "com_openf2_examples_javascript_helloworld",
-			"manifestUrl": "../apps/JavaScript/HelloWorld/manifest.js",
-			"name": "Hello World (JavaScript)"
-		},
-		{
 			"appId": "com_openf2_examples_javascript_chart",
 			"height": 300,
 			"minGridSize": 6,
@@ -26,34 +21,40 @@
 			"manifestUrl": "../apps/JavaScript/Quote/manifest.js",
 			"name": "Quote",
 			"views": ["home", "settings", "about"],
-			"minGridSize": 3
+			"minGridSize": 4
 		},
 		{
 			"appId": "com_openf2_examples_javascript_watchlist",
 			"manifestUrl": "../apps/JavaScript/Watchlist/manifest.js",
 			"name": "Watchlist",
 			"views": ["home", "settings"],
-			"minGridSize": 3
+			"minGridSize": 4
 		},
 		{
 			"appId": "com_openf2_examples_javascript_cds",
 			"manifestUrl": "../apps/JavaScript/CDS/manifest.js",
 			"name": "CDS Sovereign Big Movers",
 			"views": ["home", "about"],
-			"minGridSize": 4
+			"minGridSize": 5
+		},
+		{
+			"appId": "com_openf2_examples_javascript_compareTool",
+			"manifestUrl": "../apps/JavaScript/CompareTool/manifest.js",
+			"name": "Compare Tool",
+			"minGridSize": 12
+		},
+		{
+			"appId": "com_openf2_examples_javascript_helloworld",
+			"manifestUrl": "../apps/JavaScript/HelloWorld/manifest.js",
+			"name": "Hello World (JS)"
 		}
 	],
 
 	"php": [
 		{
-			"appId": "com_openf2_examples_php_helloworld",
-			"manifestUrl": "../apps/PHP/HelloWorld/helloworld.php",
-			"name": "Hello World (PHP)"
-		},
-		{
 			"appId": "com_openf2_examples_php_f2wits",
 			"height": 200,
-			"minGridSize": 4,
+			"minGridSize": 6,
 			"manifestUrl": "../apps/PHP/F2wits/manifest.js",
 			"name": "F2wits",
 			"views": ["home","about"]
@@ -63,14 +64,19 @@
 			"manifestUrl": "../apps/PHP/News/news.php",
 			"name": "Stock News",
 			"views": ["home", "settings"],
-			"minGridSize": 4
+			"minGridSize": 6
 		},
 		{
 			"appId": "com_openf2_examples_php_marketnews",
 			"manifestUrl": "../apps/PHP/MarketNews/marketnews.php",
 			"name": "Market News",
 			"views": ["home", "settings"],
-			"minGridSize": 4
+			"minGridSize": 6
+		},
+		{
+			"appId": "com_openf2_examples_php_helloworld",
+			"manifestUrl": "../apps/PHP/HelloWorld/helloworld.php",
+			"name": "Hello World (PHP)"
 		}
 	]
 }
