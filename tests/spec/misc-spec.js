c10b263582363931d0c34a31a30c80684af183f3
Mark Healey <mark.healey@markit.com>
2014-12-18 09:40:34 -0700

    added test for F2.version()

--- tests/spec/misc-spec.js
+++ tests/spec/misc-spec.js
@@ -8,4 +8,10 @@ describe('misc', function() {
 		}
 	});
 
+	it('F2.version() should return a version number (SemVer as String)', function() {
+		//https://github.com/sindresorhus/semver-regex
+		var testVersion = /\bv?(?:0|[1-9][0-9]*)\.(?:0|[1-9][0-9]*)\.(?:0|[1-9][0-9]*)(?:-[\da-z\-]+(?:\.[\da-z\-]+)*)?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?\b/ig.test(F2.version());
+		expect(testVersion).toBeTruthy();
+	});
+
 });
