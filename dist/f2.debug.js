8d6829043450df9de83524206819ce8661c29be2
Chris Montrois <cmontrois@quickleft.com>
2014-12-23 23:41:51 -0500

    Moving preface to header template

--- dist/f2.debug.js
+++ dist/f2.debug.js
@@ -26,12 +26,11 @@
  * in the terms and conditions related to the specific Third Party Material, if any.
  *
  */
-
 ;(function(exports) {
 
-  if (exports.F2 && !exports.F2_TESTING_MODE) {
-    return;
-  }
+	if (exports.F2 && !exports.F2_TESTING_MODE) {
+		return;
+	}
 
 /*!
  * jQuery JavaScript Library v1.11.1
@@ -13801,42 +13800,13 @@ easyXDM.stack.RpcBehavior = function(proxy, config){
 global.easyXDM = easyXDM;
 })(window, document, location, window.setTimeout, decodeURIComponent, encodeURIComponent);
 
-/*!
- * F2 v1.4.0 12-23-2014
- * Copyright (c) 2014 Markit On Demand, Inc. http://www.openf2.org
- *
- * "F2" is licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed 
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * Please note that F2 ("Software") may contain third party material that Markit 
- * On Demand Inc. has a license to use and include within the Software (the 
- * "Third Party Material"). A list of the software comprising the Third Party Material 
- * and the terms and conditions under which such Third Party Material is distributed 
- * are reproduced in the ThirdPartyMaterial.md file available at:
- * 
- * https://github.com/OpenF2/F2/blob/master/ThirdPartyMaterial.md
- * 
- * The inclusion of the Third Party Material in the Software does not grant, provide 
- * nor result in you having acquiring any rights whatsoever, other than as stipulated 
- * in the terms and conditions related to the specific Third Party Material, if any.
- *
- */
-
 var F2;
+
 /**
  * Open F2
  * @module f2
  * @main f2
  */
-
 F2 = (function() {
 
 	/**
--- dist/f2.no-third-party.js
+++ dist/f2.no-third-party.js
@@ -26,49 +26,19 @@
  * in the terms and conditions related to the specific Third Party Material, if any.
  *
  */
-
 ;(function(exports) {
 
-  if (exports.F2 && !exports.F2_TESTING_MODE) {
-    return;
-  }
-
-/*!
- * F2 v1.4.0 12-23-2014
- * Copyright (c) 2014 Markit On Demand, Inc. http://www.openf2.org
- *
- * "F2" is licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed 
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * Please note that F2 ("Software") may contain third party material that Markit 
- * On Demand Inc. has a license to use and include within the Software (the 
- * "Third Party Material"). A list of the software comprising the Third Party Material 
- * and the terms and conditions under which such Third Party Material is distributed 
- * are reproduced in the ThirdPartyMaterial.md file available at:
- * 
- * https://github.com/OpenF2/F2/blob/master/ThirdPartyMaterial.md
- * 
- * The inclusion of the Third Party Material in the Software does not grant, provide 
- * nor result in you having acquiring any rights whatsoever, other than as stipulated 
- * in the terms and conditions related to the specific Third Party Material, if any.
- *
- */
+	if (exports.F2 && !exports.F2_TESTING_MODE) {
+		return;
+	}
 
 var F2;
+
 /**
  * Open F2
  * @module f2
  * @main f2
  */
-
 F2 = (function() {
 
 	/**
