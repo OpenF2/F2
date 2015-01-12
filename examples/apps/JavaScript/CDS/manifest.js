61eea9b52434466d4bc3839f6b4fd102818421e7
Chris Montrois <cmontrois@quickleft.com>
2015-01-12 15:49:18 -0700

    Refactoring CDS example to match styling

--- examples/apps/JavaScript/CDS/manifest.js
+++ examples/apps/JavaScript/CDS/manifest.js
@@ -1,33 +1,32 @@
 F2_jsonpCallback_com_openf2_examples_javascript_cds({
-  "scripts":[
-    "../apps/JavaScript/CDS/appclass.js"
+  'scripts': [
+    '../apps/javascript/cds/scripts/appclass.js',
+    '../apps/javascript/cds/scripts/cdsformatter.js'
   ],
-  "styles":[
-    "../apps/JavaScript/CDS/app.css"
+  'styles': [
+    '../apps/javascript/cds/styles/app.css'
   ],
-  "apps":[
-    {
-      "html":[
-        '<div>',
-          '<div class="f2-app-view" data-f2-view="home">',
-            '<p class="cdsDate">',
-              '<span></span>',
-            '</p>',
-            '<div class="cdsMovers"></div>',
-            '<p>',
-              '<small>Falling (or narrowing) spreads indicate the perceived risk of default is falling. Rising (or widening) spreads indicate the perceived risk of default is rising.</small>',
-            '</p>',
-            '<p>',
-              '<small>All CDS are denominated in U.S. Dollars except U.S. sovereigns, which are in Euros.</small>',
-            '</p>',
-          '</div>',
-          '<div class="f2-app-view hide" data-f2-view="about">',
-            '<h3>About</h3>',
-            '<p>CDS Sovereign Big Movers v 1.0</p>',
-            '<p><a href="#" class="f2-app-view-trigger" data-f2-view="home">&laquo; Back</a>',
-          '</div>',
-        '</div>'
-      ].join("")
-    }
-  ]
-})
+  'apps': [{
+    'html': [
+      '<div>',
+        '<div class="f2-app-view" data-f2-view="home">',
+          '<p class="cdsDate">',
+            '<span></span>',
+          '</p>',
+          '<div class="cdsMovers"></div>',
+          '<p>',
+            '<small>Falling (or narrowing) spreads indicate the perceived risk of default is falling. Rising (or widening) spreads indicate the perceived risk of default is rising.</small>',
+          '</p>',
+          '<p>',
+            '<small>All CDS are denominated in U.S. Dollars except U.S. sovereigns, which are in Euros.</small>',
+          '</p>',
+        '</div>',
+        '<div class="f2-app-view hide" data-f2-view="about">',
+          '<h3>About</h3>',
+          '<p>CDS Sovereign Big Movers v 1.0</p>',
+          '<p><a href="#" class="f2-app-view-trigger" data-f2-view="home">&laquo; Back</a>',
+        '</div>',
+      '</div>'
+    ].join('')
+  }]
+});
---
+++ examples/apps/JavaScript/CDS/scripts/appclass.js
@@ -0,0 +1,170 @@
+/* global cdsFormatter */
+
+F2.Apps['com_openf2_examples_javascript_cds'] = (function() {
+
+  var CDS_API_URL = 'http://dev.markitondemand.com/Api/SovereignCDSMovers/jsonp';
+
+  var TABLE_ROW = [
+    '<tr>',
+      '<td class="first">',
+        '<a title="{name}" href="#" data-context="{name}" data-context-name="{name}">{name}</a>',
+      '</td>',
+      '<td>{spread}</td>',
+      '<td>{changePct}</td>',
+    '</tr>'
+  ].join('');
+
+  function supplant(text, params) {
+    return text.replace(/{([^{}]*)}/g, function(a, b) {
+      var r = params[b];
+      return typeof r === 'string' || typeof r === 'number' ? r : a;
+    });
+  }
+
+  function AppClass(appConfig, appContent, root) {
+    this.appConfig = appConfig;
+    this.appContent = appContent;
+    this.root = root;
+    this.$root = $(root);
+    this.$settings = $('form[data-f2-view=settings]', this.$root);
+    this.ui = this.appConfig.ui;
+    this.settings = {
+      allowExternalAdd: false
+    };
+
+    this.closeDate = null;
+  }
+
+  AppClass.prototype.init = function() {
+    this.ui.showMask(this.root);
+    this.getData();
+    this.initEvents();
+  };
+
+  AppClass.prototype.initEvents = function() {
+    this.$root.on('click', 'button.save', $.proxy(function() {
+      this._saveSettings();
+    }, this));
+
+    this.ui.Views.change($.proxy(function(view) {
+      if (view === F2.Constants.Views.SETTINGS) {
+        this._populateSettings();
+      }
+    }, this));
+  };
+
+  AppClass.prototype._saveSettings = function() {
+    this.settings.allowExternalAdd = $('input[name=allowExternalAdd]', this.$settings).is(':checked');
+    this.ui.Views.change(F2.Constants.Views.HOME);
+  };
+
+  AppClass.prototype._populateSettings = function() {
+    $('input[name=allowExternalAdd]', this.$settings).attr('checked', this.settings.alltableswithkeys);
+  };
+
+  AppClass.prototype.drawTable = function(tightenerRows, widenerRows) {
+    var tableHeading = [
+      '<table class="table table-condensed">',
+        '<thead>',
+          '<tr>',
+            '<th class="first">Name</th>',
+            '<th>Spread (bps)</th>',
+            '<th>1 Day<br />% Change</th>',
+          '</tr>',
+        '</thead>',
+        '<tbody>'
+    ].join('');
+
+    var tightenerTable = [
+      tableHeading,
+      tightenerRows,
+      '</tbody>',
+      '</table>'
+    ].join('');
+
+    var widenerTable = [
+      tableHeading,
+      widenerRows,
+      '</tbody>',
+      '</table>'
+    ].join('');
+
+    var tables = [
+      '<h5>Global Daily Tighteners (five-year CDS)</h5>',
+      tightenerTable,
+      '<h5>Global Daily Wideners (five-year CDS)</h5>',
+      widenerTable
+    ].join('');
+
+    $('div.cdsMovers', this.root).html(tables);
+
+    this.ui.updateHeight();
+    this.ui.hideMask(this.root);
+  };
+
+  AppClass.prototype.drawCdsList = function(useTighteners, cdsData) {
+    var label = useTighteners ? 'Tightener' : 'Widener';
+
+    if (!cdsData.length) {
+      return '<tr><td class="first" colspan="5">CDS ' + label + ' data unavailable.</td></tr>';
+    }
+
+    // Update the "last updated" date
+    if (!this.closeDate && cdsData[0].PrettyDate) {
+      this.closeDate = cdsData[0].PrettyDate;
+      $('p.cdsDate span', this.root).html(this.closeDate);
+    }
+
+    return $.map(cdsData, this.drawCdsListRow).join('');
+  };
+
+  AppClass.prototype.drawCdsListRow = function(item) {
+    item = item || {};
+
+    return supplant(TABLE_ROW, {
+      name: item.LongName,
+      spread: cdsFormatter.bps(item.ConvSpread),
+      changePct: cdsFormatter.addColorPercent(item.DailyPercentChange)
+    });
+  };
+
+  AppClass.prototype.getData = function() {
+    var tightenerData = [];
+    var widenerData = [];
+
+    $.ajax({
+      url: CDS_API_URL,
+      data: {
+        tightenersOnly: 1
+      },
+      dataType: 'jsonp',
+      context: this
+    }).done(function(jqxhr) {
+      tightenerData.push(this.drawCdsList(true, jqxhr.Data || []));
+
+      $.ajax({
+        url: CDS_API_URL,
+        data: {
+          tightenersOnly: 0
+        },
+        dataType: 'jsonp',
+        context: this
+      }).done(function(jqxhr2) {
+        widenerData.push(this.drawCdsList(false, jqxhr2.Data || []));
+      }).fail(function(jqxh2r) {
+        F2.log('OOPS. CDS Wideners data was unavailable.');
+        widenerData.push(this.drawCdsList(false, []));
+      }).always(function() {
+        this.drawTable(tightenerData, widenerData);
+      });
+    }).fail(function(jqxhr) {
+      F2.log('OOPS. CDS Tighteners data was unavailable.');
+      tightenerData.push(this.drawCdsList(true, []));
+      widenerData.push(this.drawCdsList(false, []));
+      this.drawTable(tightenerData, widenerData);
+    });
+  };
+
+  return AppClass;
+
+})();
