2f64c927344b2361c5863b5d47fd2b816722422a
Chris Montrois <cmontrois@quickleft.com>
2015-01-15 22:54:00 -0700

    Refactoring 'Quote'

--- examples/apps/JavaScript/Quote/appclass.js
+++ examples/apps/JavaScript/Quote/appclass.js
@@ -1,60 +1,51 @@
+/* global F2, $ */
+
 F2.Apps['com_openf2_examples_javascript_quote'] = function(appConfig, appContent, root) {
+
+  var QUOTE_URL = 'http://dev.markitondemand.com/Api/Quote/jsonp';
+  var LOOKUP_URL = 'http://dev.markitondemand.com/api/Lookup/jsonp';
+  var REFRESH_INTERVAL_MS = 30000;
+
+  appConfig.context = appConfig.context || {};
 
   var $root = $(root);
   var $caption = $('caption', $root);
   var $tbody = $('tbody', $root);
-  var $addToWatchlist = $('a[data-watchlist-add]', $root);
   var $settings = $('form[data-f2-view="settings"]', $root);
   var _config = {
     refreshMode: 'page',
     autoRefresh: false
   };
-  var _autoRefreshInterval = false;
+  var _autoRefreshInterval;
 
   function _getQuote(symbolData) {
     appConfig.ui.showMask($root, true);
-    appConfig.context = appConfig.context || {};
 
-    if (!!symbolData) {
+    if (symbolData) {
       appConfig.context.symbol = symbolData.symbol;
     } else if (appConfig.context.symbol) {
       appConfig.context.symbol = appConfig.context.symbol;
     } else {
-      appConfig.context.symbol = 'MSFT'; //default to Microsoft
+      appConfig.context.symbol = 'MSFT';
     }
 
     $.ajax({
-      url: 'http://dev.markitondemand.com/Api/Quote/jsonp',
+      url: QUOTE_URL,
       data: {
         symbol: appConfig.context.symbol
       },
       dataType: 'jsonp',
-      success: _renderQuote,
-      error: function() {},
+      success: function(data) {
+        if (data && data.Data && data.Data.Status === 'SUCCESS') {
+          _renderQuote(data.Data);
+        } else {
+          F2.log('Un problemo!');
+        }
+      },
       complete: function() {
         appConfig.ui.hideMask($root);
       }
     });
-
-    _getWatchListSymbols()
-  }
-
-  function _hasWatchListApp() {
-    return !!$("div.com_openf2_examples_javascript_watchlist").length;
-  }
-
-  function _watchListHasSymbol() {
-    return F2.inArray(appConfig.context.symbol, _getWatchListSymbols());
-  }
-
-  function _getWatchListSymbols() {
-    var list = [];
-
-    $('div.com_openf2_examples_javascript_watchlist tr[data-row]').each(function(idx, item) {
-      list.push($(item).attr('data-row'))
-    });
-
-    return list;
   }
 
   function _initTypeahead() {
@@ -62,15 +53,13 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function(appConfig, appContent
       autoFocus: true,
       minLength: 0,
       select: function(event, ui) {
-        //F2.Events.emit(F2.Constants.Events.APP_SYMBOL_CHANGE, { symbol: ui.item.value, name: ui.item.label });
         _getQuote({
           symbol: ui.item.value
         });
       },
       source: function(request, response) {
-
         $.ajax({
-          url: '//dev.markitondemand.com/api/Lookup/jsonp',
+          url: LOOKUP_URL,
           dataType: 'jsonp',
           data: {
             input: request.term
@@ -80,80 +69,85 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function(appConfig, appContent
               return {
                 label: item.Symbol + ' - ' + item.Name + ' (' + item.Exchange + ')',
                 value: item.Symbol
-              }
+              };
             }));
           }
         });
       }
     });
-  };
+  }
 
   function _populateSettings() {
     $('input[name=refreshMode][value=' + _config.refreshMode + ']', $settings).prop('checked', true);
     $('input[name=autoRefresh]', $settings).prop('checked', _config.autoRefresh);
   }
 
-  function _renderQuote(quoteData) {
-    if (quoteData && quoteData.Data && quoteData.Data.Status == 'SUCCESS') {
-      appConfig.ui.setTitle(quoteData.Data.Name);
+  function _renderQuote(data) {
+    var changePct = Format.number(data.ChangePercent, {
+      precision: 2,
+      withColors: true,
+      prefix: '(',
+      suffix: '%)'
+    });
+    var high = Format.number(data.High);
+    var last = Format.number(data.LastPrice, 2);
+    var lastChange = Format.number(data.Change, {
+      precision: 2,
+      withColors: true
+    });
+    var low = Format.number(data.Low);
+    var marketCap = Format.number(data.MarketCap, {
+      withMagnitude: true,
+      precision: 1
+    });
+    var open = Format.number(data.Open);
+    var volume = Format.number(data.Volume, {
+      withMagnitude: true,
+      precision: 1
+    });
 
-      $caption.promise().done(function() {
-        $(this)
-          .empty()
-          .append([
-            '<h3 class="clearfix">',
-            '<span class="last pull-left">', Format.number(quoteData.Data.LastPrice, 2), '</span>',
-            '<span class="last-change pull-right">', Format.number(quoteData.Data.Change, {
-              precision: 2,
-              withColors: true
-            }), ' ', Format.number(quoteData.Data.ChangePercent, {
-              precision: 2,
-              withColors: true,
-              prefix: '(',
-              suffix: '%)'
-            }), '</span>',
-            '</h3>'
-          ].join(''));
-      });
+    $caption.promise().done(function() {
+      var html = _captionHtml(last, lastChange, changePct);
+      $(this).empty().append(html);
+    });
 
-      $tbody.promise().done(function() {
-        $(this)
-          .empty()
-          .append([
-            '<tr>',
-            '<th>Range</th>',
-            '<td><strong>', Format.number(quoteData.Data.Low), ' - ', Format.number(quoteData.Data.High), '</strong></td>',
-            '</tr>',
-            '<tr>',
-            '<th>Open</th>',
-            '<td><strong>', Format.number(quoteData.Data.Open), '</strong></td>',
-            '</tr>',
-            '<tr>',
-            '<th>Volume</th>',
-            '<td><strong>', Format.number(quoteData.Data.Volume, {
-              withMagnitude: true,
-              precision: 1
-            }), '</strong></td>',
-            '</tr>',
-            '<tr>',
-            '<th>Market Cap</th>',
-            '<td><strong>', Format.number(quoteData.Data.MarketCap, {
-              withMagnitude: true,
-              precision: 1
-            }), '</strong></td>',
-            '</tr>'
-          ].join(''));
-      });
+    $tbody.promise().done(function() {
+      var html = _quoteTableBodyHtml(low, high, open, volume, marketCap);
+      $(this).empty().append(html);
+    });
 
-      $('span', $addToWatchlist).text(quoteData.Data.Symbol);
-      $addToWatchlist
-        .data('watchlist-add', quoteData.Data.Symbol)
-        .closest('tr').toggleClass('hide', (!_hasWatchListApp() || _watchListHasSymbol()));
+    appConfig.ui.setTitle(data.Name);
+  }
 
-    } else {
-      F2.log('Un problemo!');
-    }
-  };
+  function _captionHtml(last, lastChange, changePct) {
+    return [
+      '<h3 class="clearfix">',
+        '<span class="last pull-left">', last, '</span>',
+        '<span class="last-change pull-right">', lastChange, ' ', changePct, '</span>',
+      '</h3>'
+    ].join('');
+  }
+
+  function _quoteTableBodyHtml(low, high, open, volume, marketCap) {
+    return [
+      '<tr>',
+        '<th>Range</th>',
+        '<td><strong>', low, ' - ', high, '</strong></td>',
+      '</tr>',
+      '<tr>',
+        '<th>Open</th>',
+        '<td><strong>', open, '</strong></td>',
+      '</tr>',
+      '<tr>',
+        '<th>Volume</th>',
+        '<td><strong>', volume, '</strong></td>',
+      '</tr>',
+      '<tr>',
+        '<th class="market-cap">Market Cap</th>',
+        '<td><strong>', marketCap, '</strong></td>',
+      '</tr>'
+    ].join('');
+  }
 
   function _saveSettings() {
     clearInterval(_autoRefreshInterval);
@@ -162,11 +156,12 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function(appConfig, appContent
     _config.autoRefresh = $('input[name=autoRefresh]', $settings).prop('checked');
 
     if (_config.autoRefresh) {
-      F2.log('beginning refresh');
+      F2.log('Beginning refresh');
+
       _autoRefreshInterval = setInterval(function() {
-        F2.log('refreshed');
+        F2.log('Refreshed');
         _getQuote();
-      }, 30000);
+      }, REFRESH_INTERVAL_MS);
     }
 
     appConfig.ui.Views.change(F2.Constants.Views.HOME);
@@ -184,103 +179,89 @@ F2.Apps['com_openf2_examples_javascript_quote'] = function(appConfig, appContent
       prefix: '',
       suffix: ''
     };
+
     var _magnitudes = {
       'shortcap': ['', 'K', 'M', 'B', 'T']
     };
 
+    function _signLabel(value) {
+      if (value > 0) {
+        return 'positive';
+      } else if (value < 0) {
+        return 'negative';
+      }
+
+      return 'unchanged';
+    }
+
     return {
       /**
-       * Formats a number
-       * @method number
-       * @param {number} raw The number to format
-       * @param {object|int} [options] If int, formats to X precision. If
-       * object, formats according to options passed
-       */
+        Formats a number
+        @method number
+        @param {number} raw The number to format
+        @param {object|number} options If int, formats to X precision. If
+        object, formats according to options passed
+        @returns {string} The formatted output
+      */
       number: function(raw, options) {
         if (!raw) {
           return '--';
         }
 
-        options = typeof options === 'number' ? {
-          precision: options
-        } : options;
+        options = (typeof options === 'number') ? { precision: options } : options;
         options = $.extend({}, _defaultOptions, options);
-
-        var val;
 
         if (options.withMagnitude) {
           var c = 0;
           raw = Math.abs(raw);
+
           while (raw >= 1000 && c < 4) {
             raw /= 1000;
             c++;
           }
+
           options.magnitudeType = options.magnitudeType || 'shortcap';
           options.suffix = _magnitudes[options.magnitudeType][c];
         }
 
-        val = raw.toFixed(options.precision);
-        val = options.prefix + val + options.suffix;
+        var val = options.prefix + raw.toFixed(options.precision) + options.suffix;
 
-        return !!options.withColors
-          ? ('<span class="' + (raw > 0 ? 'positive' : raw < 0 ? 'negative' : 'unchanged') + '">' + val + '</span>')
-          : val;
+        if (options.withColors) {
+          val = '<span class="' + _signLabel(raw) + '">' + val + '</span>';
+        }
+
+        return val;
       }
     };
   })();
 
   return {
     init: function() {
-      // bind container symbol change
-      F2.Events.on(
-        F2.Constants.Events.CONTAINER_SYMBOL_CHANGE, function(symbolData) {
-          if (_config.refreshMode == 'page') {
-            _getQuote(symbolData);
-          }
+      // Bind container symbol change
+      F2.Events.on(F2.Constants.Events.CONTAINER_SYMBOL_CHANGE, function(symbolData) {
+        if (_config.refreshMode === 'page') {
+          _getQuote(symbolData);
         }
-      );
+      });
 
-      // bind app symbol change
-      F2.Events.on(
-        F2.Constants.Events.APP_SYMBOL_CHANGE, function(symbolData) {
-          if (_config.refreshMode == 'app') {
-            _getQuote(symbolData);
-          }
+      // Bind app symbol change
+      F2.Events.on(F2.Constants.Events.APP_SYMBOL_CHANGE, function(symbolData) {
+        if (_config.refreshMode === 'app') {
+          _getQuote(symbolData);
         }
-      );
+      });
 
-      // bind view change
+      // Bind view change
       appConfig.ui.Views.change(function(view) {
         if (view === F2.Constants.Views.SETTINGS) {
           _populateSettings();
         }
       });
 
-      //Talk to External Watchlist App
-      $root.on("click", "a[data-watchlist-add]", function(e) {
+      // Bind save settings
+      $root.on('click', 'button.save', _saveSettings);
 
-        if (!_hasWatchListApp()) {
-          appConfig.ui.Modals.alert("The Watchlist App is not on this container.");
-        } else {
-
-          F2.Events.emit(
-            "F2_Examples_Watchlist_Add",
-            {
-              symbol: $(this).data("watchlist-add")
-            }
-          );
-
-          $(this).closest('tr').addClass('hide');
-        }
-      });
-
-      // bind save settings
-      $root.on("click", "button.save", _saveSettings);
-
-      // init typeahead
       _initTypeahead();
-
-      // get quote
       _getQuote();
     }
   };
