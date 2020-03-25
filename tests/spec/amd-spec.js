fcdb38b3a491368a67da94feab5c07b445e15015
Brian Baker <brian.baker@markit.com>
2020-03-25 11:58:03 -0600

    update amd tests

--- tests/spec/amd-spec.js
+++ tests/spec/amd-spec.js
@@ -1,23 +1,11 @@
 describe('AMD', function() {
 
-	it('should define F2', function() {
-
-		var isLoaded = false;
-
+	it('should define F2 both as an AMD module and global', function(done) {
 		require(["../sdk/f2.min.js"], function (nonGlobalF2) {
-			isLoaded = typeof nonGlobalF2 !== "undefined";
+			expect(nonGlobalF2).toBeDefined();
+			expect(window.F2).toBeDefined();
+			done();
 		});
-
-		waitsFor(function() {
-			//console.log('ping');
-			return isLoaded;
-		}, 'F2-AMD never loaded', 10000);
-
-		runs(function() { });
 	});
 
-	it('should still globally define F2', function() {
-		expect(F2).toBeDefined();
-	});
-	
 });
